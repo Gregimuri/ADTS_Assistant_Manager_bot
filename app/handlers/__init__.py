e11954b0180c3be97dd0b92b89fc25cbf441d609
Григорий Титков <titkov@adts.su>
2026-09-03 11:48:31 +0300

    Добавить пинг всех участников группы по @all
    В любой группе на сообщение с @all бот отвечает упоминаниями известных участников и администраторов.

--- app/handlers/__init__.py
+++ app/handlers/__init__.py
@@ -3,6 +3,7 @@ from app.handlers.region_transfer import router as region_transfer_router
 from app.handlers.do_report import router as do_report_router
 from app.handlers.emm_invoice import router as emm_invoice_router
 from app.handlers.info_tt import router as info_tt_router
+from app.handlers.mention_all import router as mention_all_router
 from app.handlers.menu import router as menu_router
 from app.handlers.start import router as start_router
 from app.handlers.to_invoice import router as to_invoice_router
@@ -16,4 +17,5 @@ __all__ = [
     "do_report_router",
     "admin_reports_router",
     "region_transfer_router",
+    "mention_all_router",
 ]
